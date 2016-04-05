b04da6205021eed22c9623922847df7368c2bda1
superfatfat <z0928132189@yahoo.com.tw>
2016-04-05 09:53:26 +0800

    更新

--- app/src/main/java/com/fatfat/hw1/MainActivity.java
+++ app/src/main/java/com/fatfat/hw1/MainActivity.java
@@ -1,49 +1,76 @@
 package com.fatfat.hw1;
 
-import android.app.ActionBar;
-import android.media.Image;
-import android.support.v7.app.AppCompatActivity;
+import android.gesture.Gesture;
+import android.gesture.GestureLibraries;
+import android.gesture.GestureLibrary;
+import android.gesture.GestureOverlayView;
+import android.gesture.Prediction;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageSwitcher;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.ViewSwitcher;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
-    int[] images ={R.drawable.dino, R.drawable.pig, R.drawable.pink, R.drawable.punk, R.drawable.witch};
-    ImageSwitcher imgSwitch;
-    TextView textView;
-    int count=0;
+    private int[] images ={R.drawable.dino, R.drawable.pig, R.drawable.pink, R.drawable.punk, R.drawable.witch};
+    private ImageSwitcher imgSwitch;
+    private TextView textView;
+    private GestureOverlayView gov;
+    private int count=0;
+    private GestureLibrary gestureLibrary;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gestureLibrary = GestureLibraries.fromRawResource(this, R.raw.gestures);
+        if(!gestureLibrary.load()){
+            finish();
+        }
         Button backBtn = (Button)findViewById(R.id.backBtn);
         Button goBtn = (Button)findViewById(R.id.goBtn);
         imgSwitch = (ImageSwitcher)findViewById(R.id.imageSwitcher);
         textView = (TextView)findViewById(R.id.textView);
-        //show();
+        gov = (GestureOverlayView)findViewById(R.id.gov);
+
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                count--;
-                if (count <= 0)
-                    count = 0;
-                show();
+                back();
             }
         });
+
         goBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                count++;
-                if(count >= images.length-1)
-                    count =images.length-1;
-                show();
+                next();
+            }
+        });
+        gov.addOnGesturePerformedListener(new GestureOverlayView.OnGesturePerformedListener() {
+            @Override
+            public void onGesturePerformed(GestureOverlayView overlay, Gesture gesture) {
+                ArrayList<Prediction> predictions = gestureLibrary.recognize(gesture);
+                String gestureName = predictions.get(0).name;
+                double gestureScore = predictions.get(0).score;
+
+                switch (gestureName) {
+                    case "swipe_left":
+                        if (gestureScore >= 20) {
+                            back();
+                        }
+                        break;
+                    case "swipe_right":
+                        if (gestureScore >= 20) {
+                            next();
+                        }
+                        break;
+                }
             }
         });
         imgSwitch.setFactory(new ViewSwitcher.ViewFactory() {
@@ -65,5 +92,17 @@ public class MainActivity extends AppCompatActivity {
     private void show(){
         imgSwitch.setImageResource(images[count]);
         textView.setText((count+1)+"/"+ images.length);
+    }
+    private void back(){
+        count--;
+        if (count <= 0)
+            count = 0;
+        show();
+    }
+    private void next(){
+        count++;
+        if(count >= images.length-1)
+            count =images.length-1;
+        show();
     }
 }
